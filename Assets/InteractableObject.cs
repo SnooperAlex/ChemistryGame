fbdf05a8aece704a5dc2976ccc1489cce2d1f185
Snooper <alexg6@yahoo.com>
2023-02-19 23:42:35 +0000

    C and H atoms added

---
+++ Assets/InteractableObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public string itemName;
+
+    public string GetItemName()
+    {
+        return itemName;
+    }
+}
