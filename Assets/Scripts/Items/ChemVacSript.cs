782a480786d0be1646ae5a1950308f2a2995948c
Snooper <alexg6@yahoo.com>
2023-04-06 13:23:09 +0100

    inventory stuff

--- Assets/Scripts/Items/ChemVacSript.cs
+++ Assets/Scripts/Items/ChemVacSript.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ChemVacSript : Instruments
 {
+    public GameObject ui;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ui.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ui.SetActive(true);
+        }
         
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ui.SetActive(false);
+        }
     }
 }
